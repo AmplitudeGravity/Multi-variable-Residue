21c8a6a8666300dfe9a63d0f62195fb528d6c5b2
gang chen (陈刚) <48633803+gangchern@users.noreply.github.com>
2022-06-01 07:28:28 +0100

    Update MultiResidue.jl
    degree of a rational function is added

--- MultiResidue.jl
+++ MultiResidue.jl
@@ -1,12 +1,33 @@
-using Symbolics
-@variables x y
-f=x^2 *y
-function degree(f::Num)
-    ls=Symbolics.value(f).dict;
-    ls|>values|>collect|>sum
+using SymEngine
+@vars x y z my
+function degree(f::Basic)
+      if SymEngine.get_symengine_class(f)==:Add
+             ls=get_args(f); 
+             degvec = Array{Int64}(undef, length(ls))
+             for i=1:length(ls)
+                  degvec[i]=degree(ls[i])
+             end
+             maximum(degvec)
+      elseif SymEngine.get_symengine_class(f)==:Mul;
+            ls=get_args(f); 
+             degvec = Array{Int64}(undef, length(ls))
+             for i=1:length(ls)
+                  degvec[i]=degree(ls[i])
+             end
+             sum(degvec)
+      elseif SymEngine.get_symengine_class(f)==:Pow
+            ls=get_args(f); 
+            ls[2]*degree(ls[1])
+      elseif SymEngine.get_symengine_class(f)==:Symbol
+            1
+      else 
+            0
+      end
 end
 
+f=y^5*x^3//(z^2+1)+x^4-y^4-z*x*y+2//3
 degree(f)
+
 
 
 
